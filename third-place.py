1ba2eb4887faaa4901169e40d3d5dcc36fa6e035
macbook pro <ahmad.fight@gmail.com>
2022-08-29 06:54:25 +0700

    third places

---
+++ third-place.py
@@ -0,0 +1,15 @@
+def third_place(names, scores):
+    nilai_urut = sorted(scores, reverse=True)
+    nilai_urut = list(dict.fromkeys(nilai_urut))
+    third_place = nilai_urut[2]
+    winner = []
+    for name, score in zip(names, scores):
+        if third_place == score:
+            winner.append(name)
+    print(f"the third place are"," and ".join(winner))    
+
+
+names = ['andi', 'iman', 'budi', 'leo', 'grace']
+scores = [90, 80, 80, 100, 90]
+
+third_place(names, scores)
